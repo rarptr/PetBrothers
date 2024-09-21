364310cdf8e7b1f485fae24445be7e057faf6acc
davidrar <>
2024-09-21 05:04:56 +0300

    added pet model and result template

---
+++ src/PetBrothers.Domain/Pet.cs
@@ -0,0 +1,103 @@
+﻿using PetBrothers.Domain.Abstractions;
+
+namespace PetBrothers.Domain;
+
+/// <summary>
+/// Домашний питомец
+/// </summary>
+public class Pet
+{
+    private readonly List<PaymentDetails> _paymentDetails = [];
+
+    /// <summary>
+    /// Id
+    /// </summary>
+    public Guid Id { get; private set; }
+
+    /// <summary>
+    /// Кличка
+    /// </summary>
+    public string Name { get; private set; } = string.Empty;
+
+    /// <summary>
+    /// Вид животного
+    /// </summary>
+    public string Species { get; private set; } = string.Empty;
+
+    /// <summary>
+    /// Общее описание
+    /// </summary>
+    public string Description { get; private set; } = string.Empty;
+
+    /// <summary>
+    /// Порода
+    /// </summary>
+    public string Breed { get; private set; } = string.Empty;
+
+    /// <summary>
+    /// Окрас
+    /// </summary>
+    public string Colour { get; private set; } = string.Empty;
+
+    /// <summary>
+    /// Информация о здоровье
+    /// </summary>
+    public string HealthInformation { get; private set; } = string.Empty;
+
+    /// <summary>
+    /// Адрес текущего местоположения
+    /// </summary>
+    public string CurrentAddress { get; private set; } = string.Empty;
+
+    /// <summary>
+    /// Вес
+    /// </summary>
+    public float Weight { get; private set; }
+
+    /// <summary>
+    /// Рост
+    /// </summary>
+    public float Height { get; private set; }
+
+    /// <summary>
+    /// Номер телефона для связи с владельцем
+    /// </summary>
+    public string OwnerPhoneNumber { get; private set; } = string.Empty;
+
+    /// <summary>
+    /// Указывает кастрирован ли питомец. true - кастрирован, иначе - false
+    /// </summary>
+    public bool IsCastrated { get; private set; }
+
+    /// <summary>
+    /// Дата рождения
+    /// </summary>
+    public DateOnly BirthDate { get; private set; }
+
+    /// <summary>
+    /// Указывает вакцинирован ли питомец. true - вакцинирован, иначе - false
+    /// </summary>
+    public bool IsVaccinated { get; private set; }
+
+    /// <summary>
+    /// Статус помощи
+    /// </summary>
+    public PetAssistanceStatusEnum AssistanceStatus { get; private set; }
+
+    /// <summary>
+    /// Реквизит для финансовой помощи
+    /// </summary>
+    public IReadOnlyList<PaymentDetails> PaymentDetails => _paymentDetails;
+
+    /// <summary>
+    /// Дата создания
+    /// </summary>
+    public DateTime CreationDateTime { get; private set; }
+
+    public Result AddPaymentDetails(PaymentDetails paymentDetails)
+    {
+        _paymentDetails.Add(paymentDetails);
+
+        return Result.Success();
+    }
+}
---
+++ src/PetBrothers.Domain/PetAssistanceStatusEnum.cs
@@ -0,0 +1,27 @@
+﻿namespace PetBrothers.Domain;
+
+/// <summary>
+/// Статус животного
+/// </summary>
+public enum PetAssistanceStatusEnum
+{
+    /// <summary>
+    /// Нуждается в помощи
+    /// </summary>
+    NeedsHelp,
+
+    /// <summary>
+    /// Ищет дом
+    /// </summary>
+    LookingForHome,
+
+    /// <summary>
+    /// Дом найден
+    /// </summary>
+    FoundHome,
+
+    /// <summary>
+    /// Статус не определен
+    /// </summary>
+    None
+}
