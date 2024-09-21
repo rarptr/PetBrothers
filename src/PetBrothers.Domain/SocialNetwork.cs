4fdae875145d0482a266b52ae0df4f074189124a
rarptr <>
2024-09-21 21:43:03 +0300

    add volunteer model

---
+++ src/PetBrothers.Domain/SocialNetwork.cs
@@ -0,0 +1,17 @@
+﻿namespace PetBrothers.Domain;
+
+/// <summary>
+/// Социальная сеть
+/// </summary>
+public class SocialNetwork
+{
+    /// <summary>
+    /// Название
+    /// </summary>
+    public string Title { get; set; } = string.Empty;
+    
+    /// <summary>
+    /// Ссылка
+    /// </summary>
+    public string Link { get; set; } = string.Empty;
+}
