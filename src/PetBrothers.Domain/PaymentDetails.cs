f3b77e9b0d781d26cf8f49fc024c3556b21d4242
Rar <82528804+rarptr@users.noreply.github.com>
2024-09-21 17:41:49 +0300

    added pet model and result template (#3)
    * added pet model and result template
    
    * fix DateTime
    
    * add CSharpFunctionalExtensions

---
+++ src/PetBrothers.Domain/PaymentDetails.cs
@@ -0,0 +1,17 @@
+﻿namespace PetBrothers.Domain;
+
+/// <summary>
+/// Реквизит
+/// </summary>
+public class PaymentDetails
+{
+    /// <summary>
+    /// Название реквизита
+    /// </summary>
+    public string Title { get; set; } = string.Empty;
+
+    /// <summary>
+    /// Описание деталей реквизита
+    /// </summary>
+    public string Description { get; set; } = string.Empty;
+}
